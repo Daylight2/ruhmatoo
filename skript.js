074eaf5ded318674c622d45f6c46a899f9fdc0e7
Daylight <18598676+Daylight2@users.noreply.github.com>
2022-12-05 19:39:43 +0200

    JS fixes

--- skript.js
+++ skript.js
@@ -22,3 +22,33 @@ for (var i = 0; i < links.length; i++) {
     // nii et see on kutsutud, kui leht lõpetab laadimise
     window.addEventListener("load", highlightCurrentPage);
   }
+
+
+//Kood laenatud w3c lehelt
+
+// avab külgriba ja ülekate
+function w3_open() {
+  // näita "mySidebar" elemendi stiili
+  document.getElementById("mySidebar").style.display = "block";
+  // näita "myOverlay" elemendi stiili
+  document.getElementById("myOverlay").style.display = "block";
+}
+
+// sulgeb külgriba ja ülekate
+function w3_close() {
+  // peida "mySidebar" elemendi stiil
+  document.getElementById("mySidebar").style.display = "none";
+  // peida "myOverlay" elemendi stiil
+  document.getElementById("myOverlay").style.display = "none";
+}
+
+// klõpsatud pildi näitamiseks
+function onClick(element) {
+  // muuda "img01" pildi allikaks klõpsatud element
+  document.getElementById("img01").src = element.src;
+  // näita "modal01" elemendi stiili
+  document.getElementById("modal01").style.display = "block";
+  // salvesta "caption" elemendi jaoks alt-atribuudis määratud tekst
+  var captionText = document.getElementById("caption");
+  captionText.innerHTML = element.alt;
+}
